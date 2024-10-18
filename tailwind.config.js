dda15840e36bc6adedf7cbc62281ef7bfac3b81d
hungvannguyen <nguyenvanhung08012002@gmail.com>
2024-10-18 15:03:32 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/**/*.html"],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#ffffff",
-        secondary: "#090909",
-        third: "#2AB391",
-        fourth: "#BACCE1",
-        fifth: "#131313",
-        sixth: "rgba(19 19 19,0.15)",
-        seventh: "#6D757F",
-        eighth: "#0c1828",
-        ninth: "#2AB39180",
-        tenth: "#BACCE14D",
-        eleventh: "#000000",
-        twelfth: "#7C91AA",
-        thirteenth: "#D9D9D9",
-        fourteenth: "#DADADA",
-      }
+    content: ["./views/**/*.html"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#ffffff",
+                secondary: "#090909",
+                third: "#2AB391",
+                fourth: "#BACCE1",
+                fifth: "#131313",
+                sixth: "rgba(19 19 19,0.15)",
+                seventh: "#6D757F",
+                eighth: "#0c1828",
+                ninth: "#2AB39180",
+                tenth: "#BACCE14D",
+                eleventh: "#000000",
+                twelfth: "#7C91AA",
+                thirteenth: "#D9D9D9",
+                fourteenth: "#DADADA",
+                fifteenth: "#1313134D",
+                sixteenth: "rgba(42, 179, 145, 50)",
+            },
+            variants: {
+                scale: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
 
